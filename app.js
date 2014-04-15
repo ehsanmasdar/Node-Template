d367af452c65923fbe9422fe6c583a776ea113ee
Oksisane <ehsanmasdar@gmail.com>
2014-04-15 02:15:47 +0000

    Implemented registration with cloud sql server

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var login = require('./routes/login');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-east-05.cleardb.net',
+  user     : 'bff2e14a87c6fd',
+  password : '1c2bd241',
+  database : 'heroku_20112ec37c851b9',
+});
+connection.connect(function(err) {
+  // connected! (unless `err` is set)
+});
 var app = express();
 
 // view engine setup
@@ -23,7 +33,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/styles", express.static(__dirname + '/styles'));
 app.use("/bootstrap", express.static(__dirname + '/bootstrap'));
 
+//Form
+app.post('/login/register/submit', function(req, res) {
+  var query = connection.query('INSERT INTO users SET ?', req.body, function(err, result) {
+  });
+  console.log(query.sql);
+  res.send('Registration sucessful');
 
+});
 app.use('/', routes);
 app.use('/login', login);
 /// catch 404 and forwarding to error handler
@@ -56,6 +73,4 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
-
 module.exports = app;
