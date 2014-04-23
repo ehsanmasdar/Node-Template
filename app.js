f9e602638aa32e32f71e44e4f4b121bc690cc4c8
mcbarre12 <mcbarre12@vm-1.mcbarre12.koding.kd.io>
2014-04-23 18:23:18 +0000

    split up app.js into 2 seperate files (now using /routes/login.js for registration and login) included code for second database for testing purposes (cannot properly access openshift database)

--- app.js
+++ app.js
@@ -8,20 +8,28 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var login = require('./routes/login');
 var mysql      = require('mysql');
-var crypto = require('crypto');
-var connection = mysql.createConnection({
-  host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
-  user     : 'adminlyjUE1R',
-  password : 'nnLHs3XMiatd',
-  database : 'nodejs',
-});
+var connection;
+if (typeof process.env.OPENSHIFT_NODEJS_PORT != "undefined"){
+    connection = mysql.createConnection({
+      host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+      user     : 'adminlyjUE1R',
+      password : 'nnLHs3XMiatd',
+      database : 'nodejs',
+    });
+}
+else{
+    connection = mysql.createConnection({
+      host     : 'us-cdbr-east-05.cleardb.net',
+      user     : 'bff2e14a87c6fd',
+      password : '1c2bd241',
+      database : 'heroku_20112ec37c851b9',
+    });
+}
 connection.connect(function(err) {
   // connected! (unless `err` is set)
 });
 var app = express();
 var debug = require('debug')('my-application');
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,76 +45,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/styles", express.static(__dirname + '/styles'));
 app.use("/bootstrap", express.static(__dirname + '/bootstrap'));
 
-//Form
-app.post('/login/register/submit', function(req, res) {
-  var passHash = require('password-hash');
-  req.body.password = passHash.generate(req.body.password);
-  req.body.confirmPass = null;
-  var newBody = {
-  username: req.body.username,
-  realname: req.body.realname,
-  password: req.body.password,
-  updates: req.body.updates ? 1 : 0,
-  email: req.body.email
-  };
-  var query = connection.query('INSERT INTO users SET ?', newBody, function(err, result) {
-  });
-  console.log(query.sql);
-  res.send('Registration sucessful');
-  var nodemailer = require("nodemailer");
-
-// create reusable transport method (opens pool of SMTP connections)
-    var token = "";
-    require('crypto').randomBytes(48, function(ex, buf) {
-         token = buf.toString('hex');
-             console.log(token);
-         var smtpTransport = nodemailer.createTransport("SMTP",{
-         service: "Gmail",
-        auth: {
-            user: "duedatesstaff@gmail.com",
-            pass: "lasa2k16"
-        }
-        });
-        var url  = "http://duedates.ehsandev.com/token?";
-        console.log(token);
-        url = url + token;
-        console.log(url);
-    // setup e-mail data with unicode symbols
-        var mailOptions = {
-            from: "DueDateStaff <duedatesstaff@gmail.com>", // sender address
-            to: req.body.email, // list of receivers
-            subject: "Registration", // Subject line
-            text: "Click on the following link to complete your registration", // plaintext body
-            html: "<b>Click on the following link to complete your registration:</b>" + url // html body
-        }
-    
-        // send mail with defined transport object
-        smtpTransport.sendMail(mailOptions, function(error, response){
-            if(error){
-                console.log(error);
-            }else{
-                console.log("Message sent: " + response.message);
-            }
-        smtpTransport.close();
-        });
-
-    });
-});
-
-app.post('/login/submit', function(req, res) {
-    var passHash = require('password-hash');
-    var connect = connection.query('SELECT * FROM users WHERE username = \'' + req.body.username + '\'', function(err,rows,fields){
-      console.log(connect.sql);
-       if(err) throw err; 
-       if(rows[0] && rows[0].username == req.body.username && passHash.verify(req.body.password, rows[0].password)){
-           res.send('Login Sucessful');
-       }
-       else{
-           res.send('Login Failed');
-       }
-    });
-    
-});
 
 app.use('/', routes);
 app.use('/login', login);
--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,100 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql      = require('mysql');
+var crypto = require('crypto');
+var connection;
+if (typeof process.env.OPENSHIFT_NODEJS_PORT != "undefined"){
+    connection = mysql.createConnection({
+      host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+      user     : 'adminlyjUE1R',
+      password : 'nnLHs3XMiatd',
+      database : 'nodejs',
+    });
+}
+else{
+    connection = mysql.createConnection({
+      host     : 'us-cdbr-east-05.cleardb.net',
+      user     : 'bff2e14a87c6fd',
+      password : '1c2bd241',
+      database : 'heroku_20112ec37c851b9',
+    });
+}
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.render('login.html');
 });
 router.get('/register', function(req, res) {
   res.render('register.html');
+});
+
+//Form
+router.post('/register/submit', function(req, res) {
+  var passHash = require('password-hash');
+  req.body.password = passHash.generate(req.body.password);
+  req.body.confirmPass = null;
+  var newBody = {
+  username: req.body.username,
+  realname: req.body.realname,
+  password: req.body.password,
+  updates: req.body.updates ? 1 : 0,
+  email: req.body.email
+  };
+  var query = connection.query('INSERT INTO users SET ?', newBody, function(err, result) {
+  });
+  console.log(query.sql);
+  res.send('Registration sucessful');
+  var nodemailer = require("nodemailer");
+
+// create reusable transport method (opens pool of SMTP connections)
+    var token = "";
+    require('crypto').randomBytes(48, function(ex, buf) {
+         token = buf.toString('hex');
+         var smtpTransport = nodemailer.createTransport("SMTP",{
+         service: "Gmail",
+        auth: {
+            user: "duedatesstaff@gmail.com",
+            pass: "lasa2k16"
+        }
+        });
+        var url  = "http://duedates.ehsandev.com/token?";
+        url = url + token;
+    // setup e-mail data with unicode symbols
+        var mailOptions = {
+            from: "DueDateStaff <duedatesstaff@gmail.com>", // sender address
+            to: req.body.email, // list of receivers
+            subject: "Registration", // Subject line
+            text: "Click on the following link to complete your registration", // plaintext body
+            html: "<b>Click on the following link to complete your registration:</b>" + url // html body
+        }
+    
+        // send mail with defined transport object
+        smtpTransport.sendMail(mailOptions, function(error, response){
+            if(error){
+                console.log(error);
+            }else{
+                console.log("Message sent: " + response.message);
+            }
+        smtpTransport.close();
+        });
+
+    });
+});
+
+router.post('/submit', function(req, res) {
+    console.log("request");
+    var passHash = require('password-hash');
+    var connect = connection.query('SELECT * FROM users WHERE username = \'' + req.body.username + '\'', function(err,rows,fields){
+      console.log(connect.sql);
+       if(err) throw err; 
+       if(rows[0] && rows[0].username == req.body.username && passHash.verify(req.body.password, rows[0].password)){
+           res.send('Login Sucessful');
+       }
+       else{
+           res.send('Login Failed');
+       }
+    });
+    
 });
 
 module.exports = router;
