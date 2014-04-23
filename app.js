ec0611042c232d632cbd0327fb5d1372094e2a9f
NielsKornerup <NielssKornerup@gmail.com>
2014-04-23 17:09:37 +0000

    Email stuffs

--- app.js
+++ app.js
@@ -8,23 +8,13 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var login = require('./routes/login');
 var mysql      = require('mysql');
-if (typeof process.env.OPENSHIFT_NODEJS_PORT != "undefined"){
-
+var crypto = require('crypto');
 var connection = mysql.createConnection({
   host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
   user     : 'adminlyjUE1R',
   password : 'nnLHs3XMiatd',
   database : 'nodejs',
 });
-}
-else{
-    var connection = mysql.createConnection({
-  host     : 'us-cdbr-east-05.cleardb.net',
-  user     : 'bff2e14a87c6fd',
-  password : '1c2bd241',
-  database : 'heroku_20112ec37c851b9',
-});
-}
 connection.connect(function(err) {
   // connected! (unless `err` is set)
 });
@@ -63,7 +53,44 @@ app.post('/login/register/submit', function(req, res) {
   });
   console.log(query.sql);
   res.send('Registration sucessful');
+  var nodemailer = require("nodemailer");
 
+// create reusable transport method (opens pool of SMTP connections)
+    var token = "";
+    require('crypto').randomBytes(48, function(ex, buf) {
+         token = buf.toString('hex');
+             console.log(token);
+
+    });
+  var smtpTransport = nodemailer.createTransport("SMTP",{
+     service: "Gmail",
+    auth: {
+        user: "duedatesstaff@gmail.com",
+        pass: "lasa2k16"
+    }
+    });
+    var url  = "http://duedates.ehsandev.com/token?";
+    console.log(token);
+    url = url + token;
+    console.log(url);
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: "DueDateStaff <duedatesstaff@gmail.com>", // sender address
+    to: req.body.email, // list of receivers
+    subject: "Registration", // Subject line
+    text: "Click on the following link to complete your registration", // plaintext body
+    html: "<b>Click on the following link to complete your registration:</b>" + url // html body
+}
+
+// send mail with defined transport object
+smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+smtpTransport.close();
+});
 });
 
 app.post('/login/submit', function(req, res) {
