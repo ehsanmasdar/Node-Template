0a759bb3daada081e57679b0c868d8fb99987c16
Michael Barre <meeees13@gmail.com>
2014-05-05 12:34:22 -0500

    Attempted to redirect /register

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('login.html');
 });
+router.get('/register', function(req, res) {
+  res.redirect('login/register');
+}
 
 module.exports = router;
